dcbd861885efa6706359cc9e29bc8f7e063db76d
kasha-2 <karam.shamasyousif@gmail.com>
2020-04-30 23:42:45 -0500

    copy locations data cloud function

---
+++ functions/index.js
@@ -0,0 +1,58 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	response.send("Hello from Firebase!");
+});
+
+// Listen for changes in all documents in the 'users' collection
+exports.copyLocationsData = functions.firestore
+	.document('users/{userId}')
+	.onUpdate(async (change, context) => {
+		// If we set `/users/marie` to {name: "Marie"} then
+		// context.params.userId == "marie"
+		// ... and ...
+		// change.after.data() == {name: "Marie"}
+
+		const newStatus = change.after.data().status;
+		const userDocId = context.params.userId;
+
+		if(newStatus == 'Positive'){
+			copyData(userDocId);
+			console.log(`data copied for ` + userDocID);
+		}
+	});
+
+// Test function => http://localhost:5001/covid-19-5a8e7/us-central1/testMe
+exports.testMe = functions.https.onRequest(async (request, response) => {
+	copyData("dp3wj6x1yvrn");
+	response.send("end");
+});
+
+
+// Private method copyData
+const copyData = async (userDocId) => {
+	const toWait = [];
+
+	// set copy function => copies data to locations collection
+	const copyFunction = async (documentSnapshot) => {
+		await console.log(`new doc found` + documentSnapshot.ref.path);
+		let data = documentSnapshot.data();
+
+		await admin.firestore().collection('locationsNeg').add({
+			l: data.l,
+			userDocId: data.userDocId,
+			lastStatusUpdate: data.lastStatusUpdate
+		});
+	};
+
+	// read doc from locationSafe collection
+	const snapshots = await admin.firestore().collection('locations').where('userDocId', '==', userDocId).get();
+	snapshots.forEach((documentSnapshot) => {
+		toWait.push(copyFunction((documentSnapshot)));
+	});
+
+	await Promise.all(toWait);
+};
